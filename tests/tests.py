afa045f8c3acd9683d99ee7e53976e291a355458
Haley Schuhl <hschuhl@danforthcenter.org>
2018-10-22 17:00:23 -0500

    Trouble shooting with tests in hyperspectral
    There were no tests ran, so I edited the travis.yml file to match what is found in the plantcv repository. Also added a fake test to make sure it could pass tests. Added data file for images in tests. Added test images for the read_hyperspectral test.

--- tests/tests.py
+++ tests/tests.py
@@ -10,12 +10,12 @@ import plantcv.hyperspectral as hy
 # Import matplotlib and use a null Template to block plotting to screen
 # This will let us test debug = "plot"
 import matplotlib
-matplotlib.use('Template')
+matplotlib.use('Template', warn=False)
 
 TEST_DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 TEST_TMPDIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".cache")
-TEST_INPUT_HYPER = "test-hyper_raw.hdr"
 
+TEST_INPUT_HYPER = "darkReference.hdr"
 
 # ##########################
 # Tests setup function
@@ -29,11 +29,12 @@ def setup_function():
 # Tests for the main package
 # ##########################
 
-#def test_read_hyperspectral():
-#    hyperimg, bands, path, filename= hy.read_hyperspectral(TEST_INPUT_HYPER)
-#    assert len(bands)==978
+def test_read_hyperspectral():
+    hyperimg, bands, path, filename = hy.read_hyperspectral(os.path.join(TEST_DATA, TEST_INPUT_HYPER))
+    assert len(bands) == 978
 
-
+def test_fake_test():
+    assert 5 == 5
 # ##############################
 # Clean up test files
 # ##############################
