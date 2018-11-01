c7eaec459f42269cf1ca84d610cc517668ee28d1
cjrobey <calebrobey118@gmail.com>
2018-11-01 09:44:14 -0400

    input modules and units for SPICE

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ setuptools.setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['spectral'],
+    install_requires=['cvxopt', 'spectral'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
--- tests/tests.py
+++ tests/tests.py
@@ -16,6 +16,7 @@ TEST_DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 TEST_TMPDIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".cache")
 
 TEST_INPUT_HYPER = "darkReference.hdr"
+TEST_SPICE = "spice_data.npz"
 
 # ##########################
 # Tests setup function
@@ -32,6 +33,26 @@ def setup_function():
 def test_read_hyperspectral():
     hyperimg, bands, path, filename = hy.read_hyperspectral(os.path.join(TEST_DATA, TEST_INPUT_HYPER))
     assert len(bands) == 978
+
+
+def test_plantcv_spice_training():
+    spice = np.load(os.path.join(TEST_DATA, TEST_SPICE))
+    # In the SPICE algorithm, initialization parameters are randomized, these have a known output
+    init_endmem = spice['init_endmembers']
+    # Input data (HSI Image)
+    input_data = spice['input_data']
+    # Final data to test against
+    final_endmem = spice['final_endmembers']
+    final_prop = spice['final_proportions']
+    # Load parameters, turn off display
+    params = pcv.SPICE.SPICEParameters()
+    params.initEM = init_endmem
+    params.produceDisplay = 0
+    # Run the algorithm
+    endm, P = pcv.SPICE.SPICE(input_data, params)
+    # assert with a 0.01% tolerance, since the values are floats (can't do exact comparison)
+    assert np.allclose(endm, final_endmem, rtol=0.0001) and np.allclose(P, final_prop, rtol=0.0001)
+
 
 # ##############################
 # Clean up test files
