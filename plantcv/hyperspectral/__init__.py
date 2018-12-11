69c8b7a6f7165056117bba7922bfd1c699ebd470
Sanaz Jar <s.jarolmasjed@gmail.com>
2018-12-11 14:00:10 -0600

    edited

--- plantcv/hyperspectral/__init__.py
+++ plantcv/hyperspectral/__init__.py
@@ -1,9 +1,12 @@
 __version__ = 1.0
-__all__ = ['read_hyper_raw']
+__all__ = ['read_hyperspectral', 'hyperspectral2array', 'SPICE']
 
 from plantcv.hyperspectral.read_hyperspectral import read_hyperspectral
 
 # add new functions to end of lists
+from plantcv.hyperspectral.read_hyperspectral import read_hyperspectral
+from plantcv.hyperspectral.hyperspectral2array import hyperspectral2array
+from plantcv.hyperspectral import SPICE
 
 
 
