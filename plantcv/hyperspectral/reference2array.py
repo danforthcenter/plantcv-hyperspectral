8b4c2de873b4dd3b14ddb461b9dd365fe4df585e
Sanaz Jar <s.jarolmasjed@gmail.com>
2019-04-04 13:54:57 -0500

    updated

--- plantcv/hyperspectral/reference2array.py
+++ plantcv/hyperspectral/reference2array.py
@@ -1,79 +1,58 @@
+# read reference image
+
+import os
+import numpy as np
 from plantcv.plantcv import fatal_error
 from plantcv.plantcv import params
-import os
-from spectral import *
 from osgeo import gdal
-from osgeo.gdalconst import *
-import numpy as np
+from spectral import *
 
-def reference2array(path):
-    """this function allows you read in hyperspectral reference in raw format and returns it as array that is averaged
-    (this will be used to normalize the raw hyperspectral image)
+
+
+
+def reference2array(path_file):
+
+    """Reads the hyperspectral image and converts it to array, reads the metadata and extracts the wavelength
+
     Inputs:
-    path     = path to the raw file of reference
+    path_file      = path to the reference file
 
     Returns:
-    image_array_all = hyperspectral reference image in array format
-    gdalhyper = hyperspectral reference image
-    pixelWidth = pixelWidth
-    cols = number of cols of raw image
-    rows = number of rows of raw image
-    bands = number of bands of raw image
+    reference_array_ave = reference in array format
 
 
-    :param hyperimg: spectral object
-    :param bands: list of band centers
-    :param path: string
-    :return filname: string
+    :param path_file: str
+    :return reference_array_ave: numpy.ndarray
+
     """
-
     params.device += 1
 
+    if os.path.isfile(path_file) == False:
+        fatal_error(str(path_file) + " does not exist")
 
-    if os.path.isfile(path) == False:
-        fatal_error(str(path) + " does not exist")
+    if path_file is None:
+        fatal_error("Failed to open " + path_file)
 
-    gdalhyper = gdal.Open(path, GA_ReadOnly)
-    if gdalhyper is None:
-        print ("Couldn't open this file: " + path)
-        sys.exit("Try again!")
-    else:
-        print ("%s opened successfully" %path)
-        print ('Get image size')
-        cols = gdalhyper.RasterXSize
-        rows = gdalhyper.RasterYSize
-        bands = gdalhyper.RasterCount
-        print ("columns: %i" %cols)
-        print ("rows: %i" %rows)
-        print ("bands: %i" %bands)
-        print ('Get georeference information')
-        geotransform = gdalhyper.GetGeoTransform()
-        originX = geotransform[0]
-        originY = geotransform[3]
-        pixelWidth = geotransform[1]
-        pixelHeight = geotransform[5]
-        print ("origin x: %i" %originX)
-        print ("origin y: %i" %originY)
-        print ("width: %2.2f" %pixelWidth)
-        print ("height: %2.2f" %pixelHeight)
-        # Set pixel offset.....
-        print ('Convert image to 2D array')
-        band = gdalhyper.GetRasterBand(1)
-        image_array = band.ReadAsArray(0, 0, cols, rows)
-        image_array_name = path
-        print (type(image_array))
-        print (image_array.shape)
+    # reads hyperspectral data
+    gdalreference = gdal.Open(path_file)
+
+    # converts hyperspectral data to array
+    #reference_array = gdalreference.ReadAsArray()
+
+    # extracts shape of the array
+    cols = gdalreference.RasterXSize
+    rows = gdalreference.RasterYSize
+    bands = gdalreference.RasterCount
+
     output_list = []
     for i in range(1,bands+1):
-        band = gdalhyper.GetRasterBand(i)
-        image_array = band.ReadAsArray(0, 0, cols, rows)
-        for y in zip(*image_array):
+        band = gdalreference.GetRasterBand(i)
+        reference_array = band.ReadAsArray(0, 0, cols, rows)
+        for y in zip(*reference_array):
             avg_reflectance = sum(y)/len(y)
             #print (avg_reflectance)
             (output_list.append( (avg_reflectance) ))
             #print (output_list)
-    image_array_ave = np.reshape(output_list, (bands, cols))
-    print ('Average image width')
-    print (image_array_ave.shape)
+    reference_array_ave = np.reshape(output_list, (bands, cols))
 
-    return image_array_all, gdalhyper, cols, rows, bands
+    return reference_array_ave
