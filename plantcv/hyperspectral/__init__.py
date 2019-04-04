a0290e8acbfe5854d557bdd53d280a652a5fb91a
Sanaz Jar <s.jarolmasjed@gmail.com>
2019-04-04 13:53:20 -0500

    updated with new functions

--- plantcv/hyperspectral/__init__.py
+++ plantcv/hyperspectral/__init__.py
@@ -1,12 +1,16 @@
 __version__ = 1.0
-__all__ = ['read_hyperspectral', 'hyperspectral2array', 'SPICE']
+__all__ = ['hyper2array'] # ['read_hyperspectral', 'hyperspectral2array', 'SPICE']
 
-from plantcv.hyperspectral.read_hyperspectral import read_hyperspectral
+#from plantcv.hyperspectral.read_hyperspectral import read_hyperspectral
 
 # add new functions to end of lists
-from plantcv.hyperspectral.read_hyperspectral import read_hyperspectral
-from plantcv.hyperspectral.hyperspectral2array import hyperspectral2array
-from plantcv.hyperspectral import SPICE
 
+#from plantcv.hyperspectral import SPICE
+from plantcv.hyperspectral.hs2array import hs2array
+from plantcv.hyperspectral.extract_channel import extract_channel
+from plantcv.hyperspectral.extract_ndvi import extract_ndvi
+from plantcv.hyperspectral.reference2array import reference2array
+from plantcv.hyperspectral.calibrate_hs import calibrate_hs
+from plantcv.hyperspectral.read_hs_gdal import read_hs_gdal
 
 
